656a9f0b3f011914ce145bd6fa42fc7934f8ae52
hala277 <hala.almasharfeh@gmail.com>
2022-04-27 23:36:01 +0300

    add Phase 2

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,13 +1,17 @@
-
+import './results.scss'
 
 const Results = (props) => {
-   
-    return(
-        <section>
-          <pre>{props.data ? JSON.stringify(this.props.data , undefined , 2) : null}</pre>
-        </section>
-    )
-    }
+
+  return (
+    <section>
+      <p>method:{props.method}</p>
+      <p>URL:{props.url}</p>
+      <div id="data">
+      <pre >{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre>
+      </div>
+    </section>
+  )
+}
 
 
 export default Results;
